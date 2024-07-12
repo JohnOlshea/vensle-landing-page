b6fe450f9171a7fdf6f180cb274e743d5b9b11bc
John <johnolshea@gmail.com>
2024-07-12 10:25:24 +0100

    finish testimonial section, working on contact us

--- script.js
+++ script.js
@@ -97,3 +97,35 @@ const observer = new IntersectionObserver((entries) => {
 })
 
 observer.observe(counter)
+
+// For Swiper
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        530: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    }
+})
